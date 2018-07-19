12f654891bc3a7a15d1e382fa1c91afa74f6ea17
Sarabjit Singh <mail.singhsarabjit@gmail.com>
2018-07-20 00:37:48 +0530

    home page changes

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -3,7 +3,8 @@ package com.qa.pages;
 public class HomePage {
 	
 	public void homePage() {
-		System.out.println("Home page method");
+		System.out.println("Home Page 2");
+		System.out.println("This print was designed to check git diff command");
 	}
 
 }
